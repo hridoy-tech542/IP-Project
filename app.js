01390829e627c50484d7cfa9c5ce15c0d3f282d8
hridoy-tech542 <arafathridoy01811@gmail.com>
2025-10-03 23:37:16 +0600

    ready for render

--- app.js
+++ app.js
@@ -59,4 +59,5 @@ app.use("/books", require("./routes/books"));
 app.use("/", adminRoutes);
 
 // Start server
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
